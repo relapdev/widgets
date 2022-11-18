df01c1756f3889f48de258e959577d1d5d1a8fd5
a.schmidt <a.schmidt@corp.mail.ru>
2022-11-18 14:00:10 +0300

    add weborama

---
+++ temp/weborama.js
@@ -0,0 +1,14 @@
+var adperfobj = {
+    account_id : 7
+    ,tracking_element_id : 14065
+    ,width : 240
+    ,height : 400
+    ,fullhost : 'wcm-ru.frontend.weborama.fr'
+    ,random : '[RANDOM]'
+    ,burst : 'auto'
+    ,imptrackers : []
+    ,customparameter : []
+    ,clicktrackers : []
+    ,publisherclick : '[PUBLISHER_TRACKING_URL]'
+};
+document.write('<scr'+'ipt language="javascript" src="https://cstatic.weborama.fr/js/advertiserv2/adperf_launch_1.0.0_scrambled.js"></scr'+'ipt>');
